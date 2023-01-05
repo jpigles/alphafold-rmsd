842950d127482686667359d007cbcd5d4adecc1a
bperkinsj <109825468+bperkinsj@users.noreply.github.com>
2023-01-05 12:14:40 -0800

    Add pdb, cif_path, and pred variables

--- project_pipeline/scripts/add_auth_chain.py
+++ project_pipeline/scripts/add_auth_chain.py
@@ -4,3 +4,9 @@ import pandas as pd
 df = pd.read_csv('../data/proteins_pdb_best.tsv', sep='\t').astype('object')
 
 auth_chains = []
+
+for i in range(len(df)):
+    pdb = df.loc[i, 'PDB']
+    cif_path = (f'../data/input/RCSB_cif/{pdb}.cif')
+    pmcif = PandasMmcif()
+    pred = pmcif.read_mmcif(cif_path)
