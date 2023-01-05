3bf537dda80be17f84b4a1a7fe956a7af0ed0a2b
bperkinsj <109825468+bperkinsj@users.noreply.github.com>
2023-01-05 12:21:31 -0800

    Renamed Chain column to Label_chain

--- project_pipeline/scripts/add_auth_chain.py
+++ project_pipeline/scripts/add_auth_chain.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 df = pd.read_csv('../data/proteins_pdb_best.tsv', sep='\t').astype('object')
 
-auth_chains = []
+df_label = df.rename(columns={'Chain': 'Label_chain'})
 
+auth_chains = []
 for i in range(len(df)):
 
     # select PDB, file path, and chain
-    pdb = df.loc[i, 'PDB']
+    pdb = df_label.loc[i, 'PDB']
     cif_path = (f'../data/input/RCSB_cif/{pdb}.cif')
-    chain = df.loc[i, 'Chain']
+    lbl_chain = df_label.loc[i, 'Label_chain']
 
     # Initialize PandasMmcif object and load mmcif file
     pmcif = PandasMmcif()
