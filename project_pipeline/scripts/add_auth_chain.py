02e216b5258ac8b8c06a0b06bcecc3ddb294caf4
bperkinsj <109825468+bperkinsj@users.noreply.github.com>
2023-01-05 12:16:04 -0800

    Add comments

--- project_pipeline/scripts/add_auth_chain.py
+++ project_pipeline/scripts/add_auth_chain.py
@@ -6,7 +6,11 @@ df = pd.read_csv('../data/proteins_pdb_best.tsv', sep='\t').astype('object')
 auth_chains = []
 
 for i in range(len(df)):
+
+    # select PDB and file path
     pdb = df.loc[i, 'PDB']
     cif_path = (f'../data/input/RCSB_cif/{pdb}.cif')
+
+    # Initialize PandasMmcif object and load mmcif file
     pmcif = PandasMmcif()
     pred = pmcif.read_mmcif(cif_path)
