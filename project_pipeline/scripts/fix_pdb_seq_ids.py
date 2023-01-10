53a1e14b9bf2e02b41c2744661bd8f36fdaa6a44
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-01-10 12:30:40 -0800

    Added sample uniprot, url, post, json result

--- project_pipeline/scripts/fix_pdb_seq_ids.py
+++ project_pipeline/scripts/fix_pdb_seq_ids.py
@@ -1,3 +1,12 @@
 import json
 import requests
 
+uniprot = 'Q16644'
+
+url = f'https://www.ebi.ac.uk/pdbe/graph-api/uniprot/:{uniprot}'
+
+req = requests.post(url=url)
+
+result = json.loads(req)
+
+print(result)
