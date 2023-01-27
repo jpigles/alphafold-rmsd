1b931e810c5af41ad10b9774ff8e040c06069ce6
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-01-27 14:25:10 -0800

    Some reordering

--- project_pipeline/scripts/correct_seq_id.py
+++ project_pipeline/scripts/correct_seq_id.py
@@ -4,10 +4,7 @@ import requests
 uniprot = 'Q16644'
 
 #To access our chain of interest, an example splice can be seen below
-
-print(req_json[uniprot]['mappings'][0]['segments'][0]['chains'])
-#initiate Pandas object
-ppdb = PandasPdb()
+# print(req_json[uniprot]['mappings'][0]['segments'][0]['chains'])
 
 # Load list of pdb files
 pdb_list = pd.read_csv('./project_pipeline/data/proteins_pdb_best.tsv', sep = '\t').astype('object')
@@ -22,18 +19,26 @@ for i in range(len(pdb_list)):
     url = f'https://www.ebi.ac.uk/pdbe/graph-api/pdbe_pages/uniprot_mapping/{pdb_id}/{ent_id}'
 
     # Send requests
+    print(f'Trying {pdb_id}...')
     req = requests.get(url=url)
     print('Status: {status} for PDB {pdb}'.format(status=req.status_code, pdb=pdb_id))
     if req.status_code != 200:
         continue
     
+    # Extract uniprot and startIndex ids, get offset (defined as author start - uniprot start)
     req_json = req.json()
 
-    # Extract uniprot and startIndex ids, get offset (defined as author start - uniprot start)
     startIndex = req_json[pdb_id]['data'][0]['residues'][0]['startIndex']
     unpStart = req_json[pdb_id]['data'][0]['residues'][0]['unpStartIndex']
     offset = startIndex - unpStart
     offsets = offsets.append(offset)
+    print(f'For {pdb_id}, the offset is {offset}')
+
+    #initiate Pandas object
+    ppdb = PandasPdb()
+
+
+
 
     
 
