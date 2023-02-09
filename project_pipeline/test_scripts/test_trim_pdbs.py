a1b231b82711b86718b9d1a08d743009ba2d3ee3
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-02-09 12:33:16 -0800

    Load gt and pred models into dfs

--- project_pipeline/test_scripts/test_trim_pdbs.py
+++ project_pipeline/test_scripts/test_trim_pdbs.py
@@ -1,16 +1,28 @@
 from biopandas.pdb import PandasPdb
 import pandas as pd
 
-pdb_list = pd.read_csv('./data/proteins_pdb_best.tsv', sep='\t').astype('object')
+pdb_list = pd.read_csv('./sample_data/sample_proteins_pdb_best.tsv', sep='\t').astype('object')
 
 for i in range(len(pdb_list)):
-    for i in range(len(df)):
-    model = df.loc(i, 'Model')
+    # skip extra rows for NMR files
+    model = pdb_list.loc(i, 'Model')
     if model != 0:
         continue
-    uniprot = df.loc(i, 'Uniprot_ID')
-    pdb = df.loc(i, 'PDB ID')
-    auth_chain = df.loc(i, 'Auth_chain')
-    label_chain = df.loc(i, 'Label_chain')
+    
+    # Define parameters for selecting files
+    uniprot = pdb_list.loc(i, 'Uniprot_ID')
+    pdb = pdb_list.loc(i, 'PDB ID')
+    auth_chain = pdb_list.loc(i, 'Auth_chain')
+    label_chain = pdb_list.loc(i, 'Label_chain')
     gt_path = f'./sample_data/input/sample_pdbs/{pdb}.pdb'
     pred_path = f'./data/output/RCSB_af_full/poly_g_6_fasta/{pdb}.fasta/ranked_0.pdb'
+
+    # gt model
+    ppdb = PandasPdb()
+    gt_file = ppdb.read_pdb(gt_path)
+    gt = gt_file['ATOM']
+
+    pred_file = ppdb.read_pdb(pred_path)
+    pred = pred_file['ATOM']
+
+    
