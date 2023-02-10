d3d5d919b997b6e254678b244412e11c733ae757
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-02-10 15:51:54 -0800

    Added hydrogen filter for gt & added non-matching gt rows to extra_atoms_gt

--- project_pipeline/test_scripts/test_trim_pdbs.py
+++ project_pipeline/test_scripts/test_trim_pdbs.py
@@ -1,6 +1,7 @@
 from biopandas.pdb import PandasPdb
 import pandas as pd
 import numpy as np
+import os
 
 pdb_list = pd.read_csv('./sample_data/sample_proteins_pdb_best.tsv', sep='\t').astype('object')
 
@@ -35,8 +36,21 @@ for i in range(len(pdb_list)):
     print(len(gt), len(pred))
 
     # Create list of rows that are present in both gt and pred based on row indices of pred
-    present_atoms = []
+    present_atoms_pred = []
+    extra_atoms_gt = []
+
+    # Define atom_names for hydrogens
+    hydrogens = ['HA', 'HB1', 'HB2', 'HB3', 'H', 'HA2', 'HA3', 'HG2', 'HG3', 'HD2', 'HD3', 'HE1', 'HE2',
+                'HE3', 'HB', 'HG11', 'HG12', 'HG13', 'HG21', 'HG22', 'HG23', 'HE', 'HH11', 'HH12', 'HH21',
+                'HH22', 'HE21', 'HE22', 'HD1', 'HZ', 'HH', 'HG1', 'HD21', 'HD22', 'HG', 'HD11', 'HD12', 
+                'HD13', 'HD23', 'HZ1', 'HZ2', 'HZ3', 'HH2']
+
     for atom in range(len(gt)):
+        # Define rows to be skipped (hydrogens or alternate conformations)
+        if gt.loc[atom, 'atom_name'] in hydrogens or gt.loc[atom, 'alt_loc'] == 'B':
+            extra_atoms_gt.append(atom)
+            continue
+
         # Define minimum  parameters to select unique rows
         gt_atom_name = gt.loc[atom, 'atom_name']
         gt_residue_name = gt.loc[atom, 'residue_name']
@@ -45,24 +59,28 @@ for i in range(len(pdb_list)):
         # Look for matching row in pred
         pred_row = pred.loc[(pred['atom_name'] == gt_atom_name) & (pred['residue_number'] == gt_residue_number) & (pred['residue_name'] == gt_residue_name)]
         if pred_row.empty != True:
-            present_atoms.append(pred_row.index)
+            present_atoms_pred.append(pred_row.index)
+        else:
+            extra_atoms_gt.append(atom)
 
     # Select all rows in pred not present in gt
     total_atoms = list(pred.index)
-    na_atoms_array = np.setdiff1d(total_atoms, present_atoms)
+    na_atoms_array = np.setdiff1d(total_atoms, present_atoms_pred)
     na_atoms = sorted(na_atoms_array)
 
     # Create new pred data frame exclusively with atoms present in gt
     pred_trim = pred.drop(index=na_atoms)
+    gt_trim = gt.drop(index=extra_atoms_gt)
 
-    print(len(gt), len(pred_trim))
-    # assert len(pred_trim) == len(gt)
+    print('Length of gt: ' + str(len(gt_trim)) + ', Length of pred: ' + str(len(pred_trim)))
 
-    # print(f'Success! Creating trimmed files for {pdb}...')
+    assert len(pred_trim) == len(gt_trim)
 
-    # new_gt = gt.to_csv(gt_out_path, sep='\t', index=False)
-    # new_pred = pred_trim.to_csv(pred_out_path, sep='\t', index=False)
-        # # pred_chain = pred.loc[pred['chain_id'] == gt_chain_id]
-# # pred_residue = pred_chain.loc[pred_chain['residue_number'] == gt_residue_number]
-# # pred_atom = pred_residue.loc[pred_residue['atom_name'] == gt_atom_name]
-# pred_trim = pred.loc[(pred['chain_id'] == gt_chain_id) & (pred['residue_number'] == gt_residue_number) & (pred['atom_name'] == gt_atom_name)]
+    print(f'Success! Creating trimmed files for {pdb}...')
+    # Make directory for specific pdb
+    try:
+        new_gt = gt.to_csv(gt_out_path, sep='\t', index=False)
+        new_pred = pred_trim.to_csv(pred_out_path, sep='\t', index=False)
+    except OSError:
+        af_dir = os.mkdir(f'./sample_data/output/ds1_af_full/poly_g_20_fasta/{pdb}.fasta/')
+        new_pred = pred_trim.to_csv(pred_out_path, sep='\t', index=False)
