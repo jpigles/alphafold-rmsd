d0c652b898a94beb8bed566de19ef9d1fccfdc86
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-02-23 13:44:25 -0800

    Minor grammatical changes.

--- project_pipeline/scripts/calculate_rmsd.py
+++ project_pipeline/scripts/calculate_rmsd.py
@@ -1,4 +1,5 @@
 from pymol import cmd
+import pandas as pd
 
 # Load and select native and pred pdbs.
 def load_and_select(gt_fn, pred_fn, region1, region2):
@@ -22,45 +23,40 @@ def superimpose_region(region_num):
     cmd.color('orange','pred_1')
     
 
-# First define what those regions are.
-# superimpose receptor chains and calculate rmsd for ligand
 def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region1, region2, verbose=False):
-        '''Calculate rmsd between gt and pred regions and whole proteins
-            Region1 is autoinhibitory region, region2 is domain
-        '''
-        
-        # Rmsds are in order of whole protein, region2, region1
-        rmsds = []
-        load_and_select \
-            (gt_pdb_fn, pred_pdb_fn,
-            region1, region2)
+    '''Calculate rmsd between gt and pred regions and whole proteins
+        Region1 is autoinhibitory region, region2 is domain
+    '''
+    
+    # Rmsds are in order of whole protein, region2, region1
+    rmsds = []
+    load_and_select \
+        (gt_pdb_fn, pred_pdb_fn,
+        region1, region2)
 
-        # Superimpose region2 (domains) and calculate rmsd for whole protein and only region2. Save complex based on region2 alignment.
-        superimpose_region(2)
-        cmd.multisave(complex_fn, 'all', format='pdb')
-        # Calculate rmsd of whole protein
-        rmsd = cmd.rms_cur('native','pred')
-        rmsds.append(rmsd)
-        # Calculate rmsd of region2
-        rmsd = cmd.rmsd_cur('native_2, pred_2')
-        rmsds.append(rmsd)
+    # Superimpose region2 (domains) and calculate rmsd for whole protein and only region2. Save complex based on region2 alignment.
+    superimpose_region(2)
+    cmd.multisave(complex_fn, 'all', format='pdb')
+    # Calculate rmsd of whole protein
+    rmsd = cmd.rms_cur('native','pred')
+    rmsds.append(rmsd)
+    # Calculate rmsd of region2
+    rmsd = cmd.rmsd_cur('native_2, pred_2')
+    rmsds.append(rmsd)
 
-        # Superimpose region1 (autoinhibitory regions) and calculate rmsd
-        superimpose_region(1)
-        rmsd = cmd.rms_cur('native_1', 'pred_1')
-        rmsds.append(rmsd)
+    # Superimpose region1 (autoinhibitory regions) and calculate rmsd
+    superimpose_region(1)
+    rmsd = cmd.rms_cur('native_1', 'pred_1')
+    rmsds.append(rmsd)
 
-        # save two objects after superimposing receptor chain
-        cmd.color('gray','native')
-        cmd.color('red','pred')
-        for obj in ['native','pred']:
-            cmd.color('yellow', f'{obj}_interface_R')
-            cmd.color('blue',f'{obj}_interface_L')
+    # save two objects after superimposing region1
+    cmd.color('gray','native')
+    cmd.color('red','pred')
+    for obj in ['native','pred']:
+        cmd.color('yellow', f'{obj}_interface_R')
+        cmd.color('blue',f'{obj}_interface_L')
 
-        rmsds = [round(rmsd,3) for rmsd in rmsds]
-        if verbose: print(rmsds)
-        return rmsds
+    rmsds = [round(rmsd,3) for rmsd in rmsds]
+    if verbose: print(rmsds)
+    return rmsds
 
-# Superimpose receptors
-
-# Calculate rmsd
