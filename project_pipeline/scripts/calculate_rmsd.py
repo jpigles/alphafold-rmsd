4ac3ee9111f5e8f692bcadda40be4444b39847dd
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-02-23 15:24:01 -0800

    Defined anchors for each region so superimpose will work.

--- project_pipeline/scripts/calculate_rmsd.py
+++ project_pipeline/scripts/calculate_rmsd.py
@@ -5,14 +5,20 @@ import csv
 pdb_df = pd.read_csv('./data/proteins_pdb_best.tsv', sep='\t').astype('object')
 
 def replace_commas(region):
-    cmd_region = region
-    if ',' in cmd_region:
-        cmd_region = cmd_region.replace(',', '+')
+    '''If a region has several sections, do two things
+        First, create full_region which just replaces the comma with + (from ###-###,###-### to ###-###+###-###)
+        Second, create anchor_region, which is just the first section (from 111-111,222-222 to 111-111) which will be used to superimpose
+        If no commas, full_region and anchor_region are the same'''
+    full_region = region
+    anchor_region = region
+    if ',' in region:
+        full_region = region.replace(',', '+')
+        anchor_region = region.split(',')[0]
         
-    return cmd_region
+    return full_region, anchor_region
 
 
-def load_and_select(gt_fn, pred_fn, region1, region2):
+def load_and_select(gt_fn, pred_fn, region_1, region_2, region1_anchor, region2_anchor):
     # Load and select native and pred pdbs
     cmd.delete('all')
     cmd.load(gt_fn, 'native')
@@ -20,21 +26,23 @@ def load_and_select(gt_fn, pred_fn, region1, region2):
 
     for obj in ['native','pred']:
         # select region1 and region2
-        cmd.select(f'{obj}_1', f'{obj} and {region1}')
-        cmd.select(f'{obj}_2', f'{obj} and {region2}')
+        cmd.select(f'{obj}_1', f'{obj} and resi {region_1}')
+        cmd.select(f'{obj}_2', f'{obj} and resi {region_2}')
+        cmd.select(f'{obj}1_anchor', f'{obj} and resi {region1_anchor}')
+        cmd.select(f'{obj}2_anchor', f'{obj} and resi {region2_anchor}')
         cmd.color('red', f'{obj}_1')
         cmd.color('green',f'{obj}_2')
 
 def superimpose_region(region_num):
     # superimpose given region and calculate rmsd
-    super = cmd.super(f'native_{region_num}',f'pred_{region_num}')
+    super = cmd.super(f'native{region_num}_anchor',f'pred{region_num}_anchor')
     cmd.color('purple','native_2')
     cmd.color('yellow','native_1')
     cmd.color('blue','pred_2')
     cmd.color('orange','pred_1')
     
 
-def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region1, region2, verbose=False):
+def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region_1, region_2, region1_anchor, region2_anchor, verbose=False):
     '''Calculate rmsd between gt and pred regions and whole proteins
         Region1 is autoinhibitory region, region2 is domain
     '''
@@ -43,7 +51,8 @@ def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region1, region2, verbose
     rmsds = []
     load_and_select \
         (gt_pdb_fn, pred_pdb_fn,
-        region1, region2)
+        region_1, region_2, 
+        region1_anchor, region2_anchor)
 
     # Superimpose region2 (domains) and calculate rmsd for whole protein and only region2. Save complex based on region2 alignment.
     superimpose_region(2)
@@ -52,7 +61,7 @@ def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region1, region2, verbose
     rmsd = cmd.rms_cur('native','pred')
     rmsds.append(rmsd)
     # Calculate rmsd of region2
-    rmsd = cmd.rmsd_cur('native_2, pred_2')
+    rmsd = cmd.rms_cur('native_2', 'pred_2')
     rmsds.append(rmsd)
 
     # Superimpose region1 (autoinhibitory regions) and calculate rmsd
@@ -64,8 +73,8 @@ def calculate_rmsd(gt_pdb_fn, pred_pdb_fn, complex_fn, region1, region2, verbose
     cmd.color('gray','native')
     cmd.color('red','pred')
     for obj in ['native','pred']:
-        cmd.color('yellow', f'{obj}_interface_R')
-        cmd.color('blue',f'{obj}_interface_L')
+        cmd.color('yellow', f'{obj}_1')
+        cmd.color('blue',f'{obj}_1')
 
     rmsds = [round(rmsd,3) for rmsd in rmsds]
     if verbose: print(rmsds)
@@ -76,16 +85,17 @@ for i in range(len(pdb_df)):
     # Define pdb, filenames, region1, region2
     pdb = pdb_df.loc[i, 'PDB ID']
     uniprot = pdb_df.loc[i, 'Uniprot_ID']
-    region_1 = replace_commas(pdb_df.loc[i, 'region_1'])
-    region_2 = replace_commas(pdb_df.loc[i, 'region_2'])
+    region_1, region1_anchor = replace_commas(pdb_df.loc[i, 'region_1'])
+    region_2, region2_anchor = replace_commas(pdb_df.loc[i, 'region_2'])
     percent_reg1 = pdb_df.loc[i, 'Percent residues in region_1']
     percent_reg2 = pdb_df.loc[i, 'Percent residues in region_2']
     gt_fn = f'./data/input/RCSB/pdbs_trim/{pdb}.pdb'
     pred_fn = f'./data/output/RCSB_af_full/af_trim/{pdb}.fasta/ranked_0.pdb'
     complex_fn = f'./data/output/RCSB_af_full/complex/{pdb}.pdb'
-
-    rmsds = calculate_rmsd(gt_fn, pred_fn, complex_fn, region_1, region_2)
-
+    
+    print(f'Trying {pdb}...')
+    rmsds = calculate_rmsd(gt_fn, pred_fn, complex_fn, region_1, region_2, region1_anchor, region2_anchor)
+    
     rmsd_dic = {'UniProt': uniprot,
                 'PDB': pdb,
                 'complex_rmsd': rmsds[0],
@@ -93,6 +103,7 @@ for i in range(len(pdb_df)):
                 'region2_rmsd': rmsds[1],
                 'Percent residues in region_1': percent_reg1,
                 'Percent residues in region_2': percent_reg2}
+    print('Success! Writing rmsds')
     rmsd_info.append(rmsd_dic)
 
 with open('./data/rmsds.tsv', 'w') as file:
