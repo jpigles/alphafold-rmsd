78f6d7a94e345180e23214c467eea875c04214a0
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-05-23 14:47:32 -0700

    Intermediate file version of proteins_by_pdbs, fixed missing parameter in correct_offset call)

--- project_pipeline/scripts/pdb_ids.py
+++ project_pipeline/scripts/pdb_ids.py
@@ -30,8 +30,11 @@ main.download_pdb_files(df_prot, cif_path)
 # Make a new dataframe with each PDB ID in a separate row and chains in their own column
 df_prot = utils.expand_on_pdbs(df_prot)
 
+# Save an intermediary version of the dataframe to a file to assist in debugging
+df_prot.to_csv(snakemake.output[0], sep='\t', index=False)
+
 # Fix any offsets between the UniProt sequence and the PDB sequence in the CIF files
-df_prot = main.correct_offset(df_prot)
+df_prot = main.correct_offset(df_prot, cif_path)
 
 # Save the dataframe as a tsv file
 df_prot.to_csv(snakemake.output[0], sep = '\t', index = False)
