1f7088683e3f5750889b163769d37d972805c000
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-05-23 13:06:19 -0700

    Created rule rmsd_plot and rule all to establish target

--- project_pipeline/scripts/rmsd_plot.py
+++ project_pipeline/scripts/rmsd_plot.py
@@ -1,21 +1,15 @@
-import matplotlib as mpl
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+import seaborn as sns
+import matplotlib as mpl
 import numpy as np
-import pandas as pd
 
-def my_plotter(ax, data1, data2, param_dict):
+df = pd.read_csv(snakemake.input[0], sep='\t').astype('object')
 
-    out = ax.plot(data1, data2, **param_dict)
-    return out
+fig, ax = plt.subplots()
+df.plot(kind='scatter', y='2.0_comp', x='2.0_aligned', ax=ax)
+ax.set_xlim([0, 25])
+ax.set(title='Rmsds of Proteins Aligned at Domain', xlabel='Domain RMSD', ylabel='Tail RMSD')
 
-df = pd.read_excel("https://github.com/chris1610/pbpython/blob/master/data/sample-salesv3.xlsx?raw=true")
-print(df.head())
-
-top_10 = (df.groupby('name')['ext price', 'quantity'].agg({'ext price': 'sum', 'quantity': 'count'})
-          .sort_values(by='ext price', ascending=False))[:10].reset_index()
-top_10.rename(columns={'name': 'Name', 'ext price': 'Sales', 'quantity': 'Purchases'}, inplace=True)
-
-plt.style.use('ggplot')
-
-print(top_10.plot(kind='barh', y="Sales", x="Name"))
+plt.savefig(snakemake.output[0])
