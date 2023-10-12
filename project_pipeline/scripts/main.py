33668b384995e21b129f4d73577f99284ee610df
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-10-12 15:10:18 -0700

    Collecting proteins with domain info from UniProtKB and calculating pae from that

--- project_pipeline/scripts/main.py
+++ project_pipeline/scripts/main.py
@@ -707,6 +707,7 @@ def mean_pae_single_domain(df, path):
     for i in range(len(df)):
         uniprot = df.loc[i, 'uniprot']
         fn = f'AF-{uniprot}-F1-predicted_aligned_error_v4.json'
+        region_1 = df.loc[i, 'region_1']
 
         # Read in json file. In case the file doesn't exist, continue. 
         try:
@@ -716,9 +717,8 @@ def mean_pae_single_domain(df, path):
 
         # We don't have the regions defined a priori, so we simply take the entirety of the protein
         # See mean_paes for region bounds definition
-        region = [[1, len(prot_array) + 1]]
 
-        mean = utils.calculate_pae_mean(prot_array, region, region)
+        mean = utils.calculate_pae_mean(prot_array, region_1, region_1)
 
         df.loc[i, 'mean_pae'] = round(mean, 3)
 
