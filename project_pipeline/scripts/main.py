3a998c0b6461da0a5cd733f100830642ff895062
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2023-11-28 14:26:40 -0800

    Mean_pae takes in affix and suffix of fn because they're different for the different sets of data

--- project_pipeline/scripts/main.py
+++ project_pipeline/scripts/main.py
@@ -663,14 +663,14 @@ def mean_plddt(df, path):
     df.drop(columns=['region_1 search', 'region_2 search'], inplace=True)
     return df
 
-def mean_paes(df, path):
+def mean_paes(df, path, affix, suffix):
     # Calculate the average pae for region 1 to region 1, region 2 to region 2, and region 1 to region 2
 
     print('Calculating mean pae...')
 
     for i in range(len(df)):
         uniprot = df.loc[i, 'uniprot']
-        fn = f'F-{uniprot}-F1-predicted_aligned_error_v3.json'
+        fn = affix + uniprot + suffix
         region_1 = df.loc[i, 'region_1']
         region_2 = df.loc[i, 'region_2']
 
