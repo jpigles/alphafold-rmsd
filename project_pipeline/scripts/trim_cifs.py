f7f0bff7bfba40744bac7aa4bef61789f3a407ff
Brooks Perkins-Jechow <bperkinsj@gmail.com>
2024-02-02 13:15:55 -0800

    Some minor fixes.

--- project_pipeline/scripts/trim_cifs.py
+++ project_pipeline/scripts/trim_cifs.py
@@ -15,11 +15,11 @@ pred_file_format = snakemake.input[6]
 df_prot = pd.read_csv(snakemake.input[7], sep = '\t')
 
 # Make the directories
-utils.make_dirs(gt_in_path, gt_trim_path, pred_in_path, pred_trim_path, best_cif_path,
-                 gt_format = gt_file_format, pred_format = pred_file_format)
+utils.make_dirs(gt_in_path, gt_trim_path, pred_in_path, pred_trim_path, best_cif_path)
 
 # Trim the files to make sure any mutated residues are not accidentally counted in our domain completeness
-trim_values, df_prot = main.trim_cifs(df_prot, gt_in_path, gt_trim_path, pred_in_path, pred_trim_path)
+trim_values, df_prot = main.trim_cifs(df_prot, gt_in_path, gt_trim_path, pred_in_path, pred_trim_path,
+                                    gt_format = gt_file_format, pred_format = pred_file_format)
 
 # Save the trim values
 df_prot.to_csv(snakemake.output[0], sep = '\t', index = False)
--- project_pipeline/test_scripts/test_trim_cf_pdb.py
+++ project_pipeline/test_scripts/test_trim_cf_pdb.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import main
+import project_pipeline.test_scripts.test_main as test_main
 import utils
 import csv
 
@@ -15,11 +15,11 @@ df_prot = pd.read_csv(snakemake.input[4], sep = '\t')
 # We need to add chain information to the dataframe
 
 # Make the directories
-utils.make_dirs(gt_in_path, gt_trim_path, pred_in_path, pred_trim_path,
-                 gt_format = '{uniprot}/{pdb}.cif', pred_format = '{filename}')
+utils.make_dirs(gt_in_path, gt_trim_path, pred_in_path, pred_trim_path)
 
 # Trim the files to make sure any mutated residues are not accidentally counted in our domain completeness
-trim_values, df_prot = main.trim_cifs(df_prot, gt_in_path, gt_trim_path, pred_in_path, pred_trim_path)
+trim_values, df_prot = test_main.trim_cf_pdb(df_prot, gt_in_path, gt_trim_path, pred_in_path, pred_trim_path,
+                                        gt_format = '{uniprot}/{pdb}.cif', pred_format = '{filename}')
 
 # Save the trim values
 df_prot.to_csv(snakemake.output[0], sep = '\t', index = False)
