3c242ea2601d605a800cfbb10917acdf885d71a1
Brooks Perkins-Jechow <bjechow@sophie.msl.ubc.ca>
2024-05-16 11:37:10 -0700

    Full data for all multi-domain and two-state Colabfold structures

--- project_pipeline/scripts/compare_afs.py
+++ project_pipeline/scripts/compare_afs.py
@@ -19,7 +19,7 @@ af_format = prefix + '{uniprot}' + suffix # File name structure
 rmsd_info = main.compare_af(df, path1, path2, path3, af_format=af_format)
 
 with open(snakemake.output[0], 'w') as file:
-    fields = ['uniprot', 'filename', 'complex_rmsd', '1.0_aligned', '1.0_comp',
+    fields = ['uniprot', 'cf_filename', 'complex_rmsd', '1.0_aligned', '1.0_comp',
                 '1.1_aligned', '1.1_comp', '1.2_aligned', '1.2_comp', '2.0_aligned', '2.0_comp',
                 '2.1_aligned', '2.1_comp', '2.2_aligned', '2.2_comp', '2.3_aligned', '2.3_comp',
                 '1_aligned', '1_comp', '2_aligned', '2_comp']
